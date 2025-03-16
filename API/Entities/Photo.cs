34fcb3b7c1bacd730a5fba91cb32e904ed4a3134
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-16 23:28:29 +0530

    Added one to many relationship to AppUser and Photo entities and added the migration

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,11 +1,18 @@
-﻿namespace API.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public required string Url { get; set; }
         public bool IsMain { get; set; }
-
         public string? PublicId { get; set; }
+
+        //Navigation prop
+        //The below properties to tell photos table to add associate photo with AppUserId from AppUser model(Uers Table)
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; } = null!;
     }
 }
