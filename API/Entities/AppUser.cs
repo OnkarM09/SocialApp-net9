3062fd515df573bcf5449d3b16c816b6abee991b
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-16 23:16:07 +0530

    Added extension method to GetAge() by passing date of birth

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.Entities;
@@ -7,26 +8,22 @@ public class AppUser
     [Key]
     public int Id { get; set; }
     public required string UserName { get; set; } = "";
-
     public byte[] PasswordHash { get; set; } = [];
-
     public  byte[] PasswordSalt { get; set; } = [];
-
     public DateOnly DateOfBirth { get; set; }
-
     public required string KnownAs { get; set; }
     public DateTime Createdd { get; set; }
-
     public DateTime LastActive { get; set; }
-
     public required string Gender { get; set; }
     public string? Introduction { get; set; }
-
     public string? Interests { get; set; }
     public string? LookingFor { get; set; }
     public required string City { get; set; }
     public required string Country { get; set; }
+    public List<Photo> Photos { get; set; } = [];
 
-    public List<Photo> Photos { get; set; }
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
-
