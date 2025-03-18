5b806f9796475ba84c5874a7085a3409460d72b0
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-18 18:03:08 +0530

    Successfully get the members from authorized user (Genuine request)

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,32 +8,14 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  private readonly http = inject(HttpClient);
+export class HomeComponent {
   registerMode: boolean = false;
-  users: any = [];
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle(): void {
     this.registerMode = !this.registerMode;
   }
 
-  cancelRegistration(value : boolean): void {
+  cancelRegistration(value: boolean): void {
     this.registerMode = value;
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5111/api/users').subscribe(({
-      next: (res: any) => {
-        console.log(res);
-        this.users = res;
-      },
-      error: (err) => {
-        console.log('error: ', err);
-      }
-    }));
   }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../../services/members.service';
+import { Member } from '../../models/member';
 
 @Component({
   selector: 'app-member-list',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+
+  private readonly memberService = inject(MembersService);
+  members: Member[] = [];
+
+  ngOnInit() {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: (res: Member[]) => {
+        this.members = res;
+      }
+    });
+  }
 
 }
