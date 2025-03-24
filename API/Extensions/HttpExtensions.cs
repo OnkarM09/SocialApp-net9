6e0ac7644bd84d12aebe28243b20daa3ab5f3b6a
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-24 21:13:48 +0530

    Added pagination extension method, pagination header to send it in the response and added UserParams class file (BE)

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,16 @@
+using System.Text.Json;
+using API.Helpers;
+
+namespace API.Extensions;
+
+public static class HttpExtensions
+{
+    public static void AddPaginationHeader<T>(this HttpResponse response, PagedList<T> data)
+    {
+        var paginationHeader = new PaginationHeader(data.CurrentPage, data.PageSize, data.TotalCount, data.TotalPages);
+        var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        response.Headers.Append("Pagination", JsonSerializer.Serialize(paginationHeader, jsonOptions));
+        //To allow pagination header to the client without getting CORS error(won't be visible to the client)
+        response.Headers.Append("Access-Control-Expose-Headers", "Pagination");
+    }
+}
