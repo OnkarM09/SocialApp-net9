be09a358d93e0cbb25e97e47429060ff74029b44
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-25 21:02:51 +0530

    Filter added in UI and the functionality is completed

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -5,11 +5,12 @@ import { MemberCardComponent } from '../member-card/member-card.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { AccountService } from '../../../services/account.service';
 import { UserParams } from '../../models/userParams';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent, PaginationModule],
+  imports: [MemberCardComponent, PaginationModule, FormsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss'
 })
@@ -19,14 +20,19 @@ export class MemberListComponent implements OnInit {
 
   userParams = new UserParams(this.accountService.currentUser());
   members: Member[] = [];
+  genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Female' }]
 
   ngOnInit() {
     this.loadMembers();
   }
 
   loadMembers() {
-
     this.memberService.getMembers(this.userParams);
+  }
+
+  resetFilter() {
+    this.userParams = new UserParams(this.accountService.currentUser());
+    this.loadMembers();
   }
 
   pageChangeEvent(event: any) {
