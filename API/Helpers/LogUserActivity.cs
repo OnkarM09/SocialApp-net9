0dd8ae5667a350bbcccbf08855d0d543c83f0fa7
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-26 19:56:39 +0530

    Added action filter as LogUserActivity and tested the same in POSTMAN and works as expected (BE)

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,26 @@
+﻿using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+            if (context.HttpContext.User.Identity?.IsAuthenticated != true) return;
+            var username = resultContext.HttpContext.User.GetUserName();
+            if (username == null) return;
+
+            //Creating scoped instance of UserRepository services as we are creating it when action
+            //filter is being executed
+            var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+            var user = await repo.GetUserByUserNameAsync(username);
+            if (user == null) return;
+
+            user.LastActive = DateTime.UtcNow;
+            await repo.SaveAllAsync();
+        }
+    }
+}
