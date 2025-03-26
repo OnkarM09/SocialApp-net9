463d4680b2c19434818f4032928d2c0d21a3cc15
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-26 23:15:21 +0530

    Added the likesController and tested the same using POSTMAN

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,50 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class LikesController(ILikeRepository likesRepository) : CustomBaseController
+    {
+        [HttpPost("{targetUserId:int}")]
+        public async Task<ActionResult> ToggleLike(int targetUserId)
+        {
+            var sourceUserId = User.GetUserId();
+            if (sourceUserId == targetUserId) return BadRequest("Cannot like yourself");
+
+            var existingLike = await likesRepository.GetUserLike(sourceUserId, targetUserId);
+         
+            if (existingLike == null)
+            {
+                var like = new UserLike
+                {
+                    SourceUserId = sourceUserId,
+                    TargetUserId = targetUserId,
+                };
+                likesRepository.AddLike(like);
+            }
+            else
+            {
+                likesRepository.DeleteLike(existingLike);
+            }
+            if (await likesRepository.SaveChanges()) return Ok();
+            return StatusCode(500, "Error while updating like");
+        }
+
+        [HttpGet("list")]
+        public async Task<ActionResult<IEnumerable<int>>> GetCurrentUserLikesIds()
+        {
+            return Ok(await likesRepository.GetCurretUserLikeIds(User.GetUserId()));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> UserLikes(string predicate)
+        {
+            var users = await likesRepository.GetUserLikes(predicate, User.GetUserId());
+            return Ok(users);
+        }
+    }
+}
