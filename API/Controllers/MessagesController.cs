a94102078eef3c17a76b9bcc9f81f5d14927e21d
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-27 23:01:26 +0530

    Added Message controller and implmented/tested AddMessage() Method

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,41 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MessagesController(
+        IMessageRepository messageRepository,
+        IUserRepository userRepository,
+        IMapper mapper) : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var userName = User.GetUserName();
+            if (userName == createMessageDto.RecipientUserName) return BadRequest("You cannot send message to yourself");
+
+            var sender = await userRepository.GetUserByUserNameAsync(userName);
+            var recipient = await userRepository.GetUserByUserNameAsync(createMessageDto.RecipientUserName);
+
+            if (sender == null || recipient == null) return BadRequest("Cannot send message at this time");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = userName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            messageRepository.AddMessage(message);
+            if (await messageRepository.SaveAllAsync()) return Ok(mapper.Map<MessageDto>(message));
+            return StatusCode(500, "Error while adding message! Please try again later.");
+        }
+    }
+}
