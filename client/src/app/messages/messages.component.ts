b942d97708eabba00f89d3ec010c55df740d4012
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-28 13:39:41 +0530

    Added the message table in message component

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,18 +1,39 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessageService } from '../../services/message.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { Message } from '../models/message';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, RouterModule, FormsModule, TimeagoModule, PaginationModule, NgSelectModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
 export class MessagesComponent implements OnInit{
   messageService = inject(MessageService);
-  container = 'Inbox';
+  container : string = "Inbox";
   pageNumber = 1;
   pageSize  = 5;
+  messageTypeList = [
+    {
+      value : 'Unread',
+      display : 'Unread'
+    },
+    {
+      value : 'Inbox',
+      display : 'Inbox'
+    },
+    {
+      value: 'Outbox',
+      display : 'Outbox'
+    }
+  ]
   ngOnInit(): void {
     this.loadMessages();
   }
@@ -21,10 +42,22 @@ export class MessagesComponent implements OnInit{
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container);
   }
 
+  getRoute(message : Message){
+    if(this.container == 'Outbox'){
+      return `/members/${message.recipientUsername}`
+    }
+    return `/members/${message.senderUsername}`
+  }
+
   pagedChangeEvent(event : any){
     if(this.pageNumber != event.page){
       this.pageNumber = event.page;
       this.loadMessages();
     }
+  }
+
+  messageTypeChange(e : any){
+    this.container = e;
+    this.loadMessages();
   }
 }
