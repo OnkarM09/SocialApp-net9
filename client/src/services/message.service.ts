e56367c9b7e99efd5e4a4d496f50b28a396d778e
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-28 12:23:23 +0530

    Added message interface and created message service in angular (FE)

---
+++ client/src/services/message.service.ts
@@ -0,0 +1,27 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { PaginatedResult } from '../app/models/pagination';
+import { Message } from '../app/models/message';
+import { setPaginationHeaders, setPaginationResponse } from '../app/helpers/paginationHeaders';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  private readonly baseUrl = environment.apiUrl;
+  private readonly http = inject(HttpClient);
+
+  paginatedResult = signal<PaginatedResult<Message[]> | null>(null);
+
+  getMessages(pageNumber: number, pageSize: number, container: string) {
+    let params = setPaginationHeaders(pageNumber, pageSize);
+    params.append("Container", container);
+    return this.http.get<Message[]>(`${this.baseUrl}messages`, {
+      observe: 'response', params
+    }).subscribe({
+      next: response => setPaginationResponse(response, this.paginatedResult)
+    });
+  }
+}
