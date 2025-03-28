cff5e771194b575afe3b4602e0bbe69c8146935b
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-29 01:30:53 +0530

    ngx-emoji-mart has been installed but not used

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,29 +1,45 @@
-import { Component, inject, input, output } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, input, output } from '@angular/core';
 import { Message } from '../../models/message';
 import { MessageService } from '../../../services/message.service';
 import { TimeagoModule } from 'ngx-timeago';
 import { FormsModule, NgForm } from '@angular/forms';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-member-messages',
   standalone: true,
-  imports: [TimeagoModule, FormsModule],
+  imports: [TimeagoModule, FormsModule, PickerComponent],
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.scss'
 })
-export class MemberMessagesComponent  {
+export class MemberMessagesComponent {
   username = input.required<string>();
   messages = input.required<Message[]>();
   updateMessages = output<Message>();
-  messageContent : string = '';
+  messageContent: string = '';
+  toggledEmoji : boolean = false;
   private readonly messageService = inject(MessageService);
 
-  sendMessage(form : NgForm){
+  constructor(private readonly eRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    if (this.toggledEmoji && !this.eRef.nativeElement.contains(event.target)) {
+      this.toggledEmoji = false;
+    }
+  }
+
+  sendMessage(form: NgForm) {
     this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
-      next : message => {
+      next: message => {
         this.updateMessages.emit(message);
         form.reset();
       }
     });
+  }
+
+  addEmoji(event: any) {
+    console.log(event)
+    this.messageContent += event.emoji.native;
   }
 }
