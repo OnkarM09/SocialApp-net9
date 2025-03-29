6f8377527dd74f804ed96589806b510f684fcc43
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-29 20:19:39 +0530

    Added online presense tracker using dictionary(temporary) (BE)

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -5,18 +5,31 @@ using Microsoft.AspNetCore.SignalR;
 namespace API.SignalR
 {
     [Authorize]
-    public class PresenceHub : Hub
+    public class PresenceHub(PresenceTracker tracker) : Hub
     {
         public override async Task OnConnectedAsync()  //When client connect to this hub
         {
+            if (Context.User == null) throw new HubException("Cannot get current user claim");
+
+            await tracker.UserConnected(Context.User.GetUserName(), Context.ConnectionId);
+
             //When connected, execute (invoke) the following method UserIsOnline
             await Clients.Others.SendAsync("UserIsOnline", Context.User?.GetUserName());
+
+            var currentUsers = await tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
+            if (Context.User == null) throw new HubException("Cannot get current user claim");
+            await tracker.UserDisconnected(Context.User.GetUserName(), Context.ConnectionId);
             //When disconnected, execute (invoke) the following method UserIsOffline
             await Clients.Others.SendAsync("UserIsOffline", Context.User?.GetUserName());
+
+            var currentUsers = await tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+
             await base.OnDisconnectedAsync(exception);
         }
     }
