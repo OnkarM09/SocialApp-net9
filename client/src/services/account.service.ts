e8b3ed872b60ff7af804aa4bd90e532c74b22b40
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-29 15:51:33 +0530

    created admin route and component and also added canActivate route guard to handle only admin or moderate are allowed to navigate.

--- client/src/services/account.service.ts
+++ client/src/services/account.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { User } from '../app/models/user';
 import { map } from 'rxjs';
 import { environment } from '../environments/environment';
@@ -13,6 +13,14 @@ export class AccountService {
   private readonly likeService = inject(LikesService);
   private readonly baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
+  roles = computed(() => {
+    const user = this.currentUser();
+    if (user?.token) {
+      const role = JSON.parse(atob(user.token.split('.')[1])).role;
+      return Array.isArray(role) ? role : [role];
+    }
+    return [];
+  })
 
   setCurrentUser(user: User) {
     localStorage['user'] = JSON.stringify(user);
