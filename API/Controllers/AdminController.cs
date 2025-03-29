6db178ae7ee6e9a8344902787faf2a803d9de93a
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-29 13:23:30 +0530

    Added API method to update the roles and tested in POSTMAN

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -24,6 +24,32 @@ namespace API.Controllers
             return Ok(users);
         }
 
+        [Authorize(Policy = "RequiredAdminRole")]
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, string roles) {
+
+            if (string.IsNullOrEmpty(roles)) return BadRequest("You must select atleast one role");
+
+            var selectedRoles = roles.Split(",").ToArray();
+
+            var user = await userManager.FindByNameAsync(username);
+
+            if (user == null) return BadRequest("User not found");
+
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded) return StatusCode(500, "Failed to add user roles");
+
+            result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded) return StatusCode(500, "Failed to remove user roles");
+
+            return Ok(await userManager.GetRolesAsync(user));
+
+        }
+
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photos-to-moderate")]
         public ActionResult GetPhotosForModeration()
