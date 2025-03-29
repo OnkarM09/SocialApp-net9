1539a1393296fcdfc8fab6f58c80fc70e5120ea6
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-29 22:25:58 +0530

    Created message hub and group to keep 2 people chat in a group

---
+++ API/SignalR/MessageHub.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Extensions;
+using Microsoft.AspNetCore.SignalR;
+using System.Runtime.InteropServices;
+
+namespace API.SignalR
+{
+    public class MessageHub(MessageRepository messageRepository) : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var otherUser = httpContext?.Request.Query["user"];
+            if(Context.User == null || string.IsNullOrEmpty(otherUser))
+            {
+                throw new Exception("Cannot join group");
+            }
+            var groupName = GetGroupName(Context.User.GetUserName(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var messages = await messageRepository.GetMessageThread(Context.User.GetUserName(), otherUser!);
+            await Clients.Group(groupName).SendAsync("RecievedMessageThread", messages);
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        private string GetGroupName(string caller, string? other)
+        {
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller} -${other}" : $"{other}-{caller}";
+        }
+    }
+}
