c84e65aeb9d4e4cafe384105df9dbcb4c0c14458
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-30 09:20:42 +0530

    Added message hub connection in messageService for messageThread

--- client/src/services/message.service.ts
+++ client/src/services/message.service.ts
@@ -4,6 +4,9 @@ import { HttpClient } from '@angular/common/http';
 import { PaginatedResult } from '../app/models/pagination';
 import { Message } from '../app/models/message';
 import { setPaginationHeaders, setPaginationResponse } from '../app/helpers/paginationHeaders';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+import { User } from '../app/models/user';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +14,40 @@ import { setPaginationHeaders, setPaginationResponse } from '../app/helpers/pagi
 export class MessageService {
 
   private readonly baseUrl = environment.apiUrl;
+  private readonly hubUrl = environment.hubsUrl;
+
   private readonly http = inject(HttpClient);
+  private readonly toastService = inject(ToastrService);
+  private hubConnection?: HubConnection;
 
   paginatedResult = signal<PaginatedResult<Message[]> | null>(null);
+  messageThread = signal<Message[]>([]);
+
+  createHubConnection(user: User, otherUserName: string) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'message?user=' + otherUserName, {
+        accessTokenFactory: () => user.token
+      }).withAutomaticReconnect()
+      .build()
+
+    this.hubConnection.start().catch(err => {
+      console.log(err);
+      this.toastService.error("Error while creating message hub connection!");
+    });
+
+    this.hubConnection.on('RecievedMessageThread', messages => {
+      this.messageThread.set(messages)
+    })
+  }
+
+  stopHubConnection() {
+    if (this.hubConnection?.state === HubConnectionState.Connected) {
+      this.hubConnection.stop().catch(err => {
+        console.log(err);
+        this.toastService.error("Error while stopping message hub connection!");
+      })
+    }
+  }
 
   getMessages(pageNumber: number, pageSize: number, container: string) {
     let params = setPaginationHeaders(pageNumber, pageSize);
@@ -25,15 +59,15 @@ export class MessageService {
     });
   }
 
-  getMessageThread(username : string){
+  getMessageThread(username: string) {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username)
   }
 
-  sendMessage(recipientUsername : string, content : string){
-    return this.http.post<Message>(`${this.baseUrl}messages`, {recipientUsername, content})
+  sendMessage(recipientUsername: string, content: string) {
+    return this.http.post<Message>(`${this.baseUrl}messages`, { recipientUsername, content })
   }
 
-  deleteMessage(id : number){
-    return this.http.delete(this.baseUrl+ 'messages/'+id);
+  deleteMessage(id: number) {
+    return this.http.delete(this.baseUrl + 'messages/' + id);
   }
 }
