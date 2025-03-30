f3b656920ab1cab80796343c0f60cf451622db60
OnkarM09 <onkarmeherwade@gmail.com>
2025-03-30 15:24:45 +0530

    Added scroll for chat

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, inject, input } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, HostListener, inject, input, ViewChild } from '@angular/core';
 import { MessageService } from '../../../services/message.service';
 import { TimeagoModule } from 'ngx-timeago';
 import { FormsModule, NgForm } from '@angular/forms';
@@ -11,7 +11,10 @@ import { PickerComponent } from '@ctrl/ngx-emoji-mart';
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.scss'
 })
-export class MemberMessagesComponent {
+export class MemberMessagesComponent implements AfterViewChecked {
+
+  @ViewChild('chatScroll') chatScroll? : any;
+
   messageService = inject(MessageService);
 
   username = input.required<string>();
@@ -20,6 +23,16 @@ export class MemberMessagesComponent {
   toggledEmoji: boolean = false;
 
   constructor(private readonly eRef: ElementRef) { }
+
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom(){
+    if(this.chatScroll){
+      this.chatScroll.nativeElement.scrollTop = this.chatScroll.nativeElement.scrollHeight;
+    }
+  }
 
   @HostListener('document:click', ['$event'])
   clickout(event: Event) {
@@ -31,6 +44,7 @@ export class MemberMessagesComponent {
   sendMessage(form: NgForm) {
     this.messageService.sendMessage(this.username(), this.messageContent).then(() => {
       form.reset();
+      this.scrollToBottom();
     })
   }
 
